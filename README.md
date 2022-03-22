202dfd594e5d2fd6a3337f241b761737924cdd50
lq990919 <1298641478@qq.com>
2022-03-22 17:53:26 +0800

    Revert "Layui前端小DEMO"
    This reverts commit fc1cf144
    
    回滚测试功能1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Git_Leaning
 第一次上传的Git本地项目 挺麻烦的![188CC742](https://user-images.githubusercontent.com/47878444/159437059-8b812b43-cf52-444c-9f4c-8c66f06f2a1a.png)
 
+回滚测试开始
---
+++ src/main/java/com/hr/test/Demo.java
@@ -0,0 +1,14 @@
+package com.hr.test;
+
+/**
+ * @author lq
+ * @date 2022-03-22 - 17:49
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+
+}
